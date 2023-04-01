28ca05cd6e045b09c9c3657584b768bbd736f82e
Bogdan Kolesnyk <bogdan.kolesnyk@gmail.com>
2023-04-02 01:12:46 +0200

    feat: updated dependencies

--- webpack/env.ts
+++ webpack/env.ts
@@ -4,7 +4,7 @@ const IS_PROD = process.env.NODE_ENV !== 'development';
 const WDS_PORT = Number(process.env.WDS_PORT) || 8081;
 const WITH_STATS = process.env.WITH_STATS === 'true';
 const ICONS_FOLDER_PATH = resolve(__dirname, '..', './src/client/assets/icons');
-const CACHE_DIR = resolve(__dirname, '..', '.webpack-cache');
+const CACHE_DIR = resolve(__dirname, '..', '.temp/.webpack-cache');
 
 export default {
   IS_PROD,
