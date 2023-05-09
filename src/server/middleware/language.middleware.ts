14d7106796da0f8b917c2f09a473eb348593037a
Bogdan Kolesnyk <bogdan.kolesnyk@gmail.com>
2023-05-09 16:25:32 +0200

    fix: samesite for lang cookie

--- src/server/middleware/language.middleware.ts
+++ src/server/middleware/language.middleware.ts
@@ -5,7 +5,10 @@ export const languageMiddleware: RequestHandler = (request, response, next) => {
   const { params } = request;
 
   if (acceptedLanguages.includes(params.lang)) {
-    response.cookie('lang', params.lang);
+    response.cookie('lang', params.lang, {
+      sameSite: 'lax',
+      secure: true,
+    });
   } else if (!params.lang) {
     return response.redirect(302, `/${getLanguage(request)}`);
   }
